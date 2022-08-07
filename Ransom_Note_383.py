e6f001284e89606e6ac466da97460dc301bc5678
Mohit Singh <107310472+mochi-momo@users.noreply.github.com>
2022-08-07 19:54:52 -0400

    Add files via upload

---
+++ Ransom_Note_383.py
@@ -0,0 +1,23 @@
+#Solve with Counter
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ran = Counter(ransomNote)
+        mag = Counter(magazine)
+    
+        for key in ran:
+            if ran[key] > mag[key]:
+                return False
+        
+        return True
+
+#Solve with list
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        mag = list(magazine)
+        for char in ransomNote:
+            if char in mag:
+                mag.remove(char)
+            else:
+                return False
+        return True

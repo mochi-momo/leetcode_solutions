e389b917e25127ec2d480cdf515d1621a0745879
Mohit Singh <107310472+mochi-momo@users.noreply.github.com>
2022-08-06 20:09:42 -0400

    Add files via upload

---
+++ Roman_to_Integer_13.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        numerals = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000} # dictionary with values
+        total = 0
+        
+        #run through each character of input
+        #if value is less than the value that comes afterwards subtract, else add
+        for x in range(len(s)):
+            if x+1 < len(s) and numerals[s[x]] < numerals[s[x+1]]:
+                total -= numerals[s[x]]
+            else:
+                total += numerals[s[x]]
+        
+        return total

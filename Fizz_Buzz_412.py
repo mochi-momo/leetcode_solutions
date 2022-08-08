ce416ecf8463aa5ad977dd4c06dc500365a48a81
Mohit Singh <107310472+mochi-momo@users.noreply.github.com>
2022-08-08 18:39:22 -0400

    Add files via upload

---
+++ Fizz_Buzz_412.py
@@ -0,0 +1,29 @@
+### Solution that appends directly to a list
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        out = []
+        for x in range(1, n+1):
+            if x % 3 == 0 and x % 5 == 0:
+                out.append("FizzBuzz")
+            elif x % 3 == 0:
+                out.append("Fizz")
+            elif x % 5 == 0:
+                out.append("Buzz")
+            else:
+                out.append(str(x))
+        return out
+
+### Solution that uses a temporary string
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        out = []
+        for x in range(1, n+1):
+            temp = ''
+            if x % 3 == 0:
+                temp += 'Fizz'
+            if x % 5 == 0:
+                temp += 'Buzz'
+            if (x % 5 != 0) and (x % 3 != 0):
+                temp += str(x)
+            out.append(temp)
+        return out

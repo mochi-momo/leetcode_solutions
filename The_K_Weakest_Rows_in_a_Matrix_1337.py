c30286e6c8f3b29f8fedec2141808dc8e92428a1
Mohit Singh <107310472+mochi-momo@users.noreply.github.com>
2022-08-09 23:41:54 -0400

    Add files via upload

---
+++ The_K_Weakest_Rows_in_a_Matrix_1337.py
@@ -0,0 +1,22 @@
+### Simple Solution
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        temp = []
+        for row, soldiers in enumerate(mat):
+            temp.append((sum(soldiers), row))
+        temp.sort()
+        
+        out = []
+        for val in temp[:k]:
+            out.append(val[1])
+        return out
+
+### Solution with a more complex but effecient return
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        temp=[]
+        for row, soldiers in enumerate(mat):
+            temp.append((sum(soldiers),row))      
+        temp.sort()
+       
+        return [val[1] for val in temp[:k]]  

ccc8b4b39a15054a2afdfdf39d8c7b5262c6a534
Mohit Singh <107310472+mochi-momo@users.noreply.github.com>
2022-08-14 22:51:15 -0400

    Add files via upload

---
+++ Valid_Parentheses_20.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {')':'(', '}':'{', ']':'['}
+        open_brackets=[]
+        for char in s:
+            if char in brackets.values():
+                open_brackets.append(char)
+            elif open_brackets and brackets[char] == open_brackets[-1]:
+                open_brackets.pop()
+            else: return False
+        return open_brackets == []
